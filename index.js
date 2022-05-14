e5e16f8674bd480cd198a8fb306363dad8188714
Freddy Febres <freddyrfa@gmail.com>
2022-05-14 15:13:02 -0400

    ✨ Adding socket.io & its functionality

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const {resolve} = require('path')
 require('dotenv').config()
 
+const app = express();
+
+// Node-socket Server.
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Socket messages.
+io.on('connection', client => {
+  console.log('Cliente conectado')
+
+  client.on('disconnect', () => console.log('cliente disconectado'));
+});
+
 const {PORT} = process.env
 
-const app = express()
+const publicPath = resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
-const publicPath = resolve(__dirname, 'public')
-app.use(express.static(publicPath))
-
-app.listen(PORT, (err) => {
+server.listen(PORT, (err) => {
   if (err) throw new Error(err)
 
   console.log('Servidor corriendo en puerto: ', PORT)
