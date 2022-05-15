978e2c01bcb3817c77cd3df7ba48d1129a275307
Freddy Febres <freddyrfa@gmail.com>
2022-05-15 11:46:00 -0400

    ✨ Last touches on server

--- sockets/index.js
+++ sockets/index.js
@@ -5,9 +5,9 @@ const Bands = require('../models/bands');
 const bands = new Bands();
 
 bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Linkin Park'));
 bands.addBand(new Band('Metallica'));
 bands.addBand(new Band('Queen'));
-bands.addBand(new Band('UB40'));
 
 io.on('connection', client => {
   console.log('Cliente conectado')
@@ -17,6 +17,22 @@ io.on('connection', client => {
   client.on('emitir-mensaje', (payload) => {
     // io.emit('nuevo-mensaje', payload) // emite a todos
     client.broadcast.emit('nuevo-mensaje', payload) // emite a todos menos el emisor.
+  });
+
+  client.on('vote-band', (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands())
+  });
+
+  client.on('add-band', (payload) => {
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit('active-bands', bands.getBands())
+  });
+
+  client.on('delete-band', (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit('active-bands', bands.getBands())
   });
 
   client.on('disconnect', () => console.log('cliente disconectado'));
